e1eb563dc99ae0f94b2e7df356ecdaf99a6c4f96
Igna695 <igna695@gmail.com>
2018-01-13 18:17:05 +0100

    Test de conexión con la base de datos

--- testArchive.js
+++ testArchive.js
@@ -3,14 +3,16 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '1234',
+  password : 'hola',
   database : 'votaciones_splc'
 });
-
+console.log('Test de conexión con la base de datos');
 connection.connect(function(err) {
   if (err) {
     console.error('Error connecting to MySQL: ' + err.stack);
+    console.log('TEST FALLIDO');
     return;
   }
    console.log('connected as id ' + connection.threadId);
+   console.log('TEST OK');
    });
