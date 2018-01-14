d672af39b013add37f4683ec206f368f09e6c87e
budia95 <antoniobudia.arb@gmail.com>
2018-01-14 12:23:49 +0100

    Realizado nuevo comando wiki

--- index.js
+++ index.js
@@ -1,71 +1,48 @@
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-
 var {WebClient} = require('@slack/client');
 var API = process.env.SLACK_TOKEN
-var ayud = "Estos son los comandos disponibles en el bot: \n\n *Buenas:* el bot te saludará cordialmente. \n *!polls:* muestra un listado de las encuestas disponibles junto con su id. \n *¿poll x:* Muestra el listado de preguntas para una encuesta. x se corresponde con el id de la encuesta.";
-
-
-
 var rtm = new RtmClient(API);
+var web = new WebClient(API);
+
 rtm.start();
 
-var web = new WebClient(API);
 let channel;
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+
   for (const c of rtmStartData.channels) {
       if (c.is_member && c.name ==='testbot') { channel = c.id }
   }
+
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
+
 });
 
 var mysql      = require('mysql');
 var queryString = 'SELECT title FROM poll';
 
 var connection = mysql.createConnection({
+
   host     : 'localhost',
   user     : 'root',
   password : '1234',
   database : 'votaciones_splc'
+
 });
 
 function getRandomColor() {
+
 	var letters = '0123456789ABCDEF';
 	var color = '#';
+
 	for (var i = 0; i < 6; i++) {
 	  color += letters[Math.floor(Math.random() * 16)];
 	}
+
 	return color;
 }
-
-var at2 ={
-	as_user: true,
-	attachments: [
-		{
-			color:"danger",
-			text:"Viajecito",
-			fallback: "Book your flights at https://flights.example.com/book/r123456",
-			actions: [
-				{
-					type: "button",
-					text: "Book flights 🛫",
-					url: "https://flights.example.com/book/r123456",
-					style: "primary",
-					value: 1
-				},
-				{
-					type: "button",
-					text: "Cancel travel request",
-					url: "https://flights.example.com/book/r123456",
-					style: "primary",
-					value: 2
-				}
-			]
-		}
-	]
-};
   
 rtm.on(RTM_EVENTS.MESSAGE, function(message) {
 	var msg = message.text;
@@ -95,47 +72,11 @@ rtm.on(RTM_EVENTS.MESSAGE, function(message) {
 		});
 	}
 
-	if(msg.includes('Benavides')||msg.includes('benavide')||msg.includes('benavides')){
-		rtm.sendMessage('Be careful my friend <@'+message.user+'>. El profesor podría estar en esta sala.', channel);
-	}
-
 	if(msg==='Buenas' || msg==='Hola'){
-
-	web.chat.postMessage(API, channel, 'hola');
-	rtm.sendMessage('Buenas <@'+message.user+'>. Espero que tenga un buen día', channel);
-	}
-	
-	if(msg==='button'){
-		var at1 ={
-			as_user: true,
-			  attachments: [
-				{
-					color:'danger',
-					text:"Viajecito",
-					fallback: "Book your flights at https://flights.example.com/book/r123456",
-					actions: [
-						{
-							type: "button",
-							text: "Book flights 🛫",
-							url: "https://flights.example.com/book/r123456",
-							style: "primary"
-						},
-						{
-							type: "button",
-							text: "Cancel travel request",
-							style: "primary",
-							url: "https://requests.example.com/cancel/r123456",
-						}
-					]
-				}
-			]
-		};
-
-		return	web.chat.postMessage(message.channel, '', at);
-	
+		rtm.sendMessage('Buenas <@'+message.user+'>. Espero que tenga un buen día', channel);
 	}
 
-	if(msg.includes('¿poll')){
+	if(msg.startsWith('¿poll')){
 
 		var pollid=msg.substring(6,msg.length);
 
@@ -166,10 +107,94 @@ rtm.on(RTM_EVENTS.MESSAGE, function(message) {
 
 	}
 
-	if (msg=='!help'){
-		rtm.sendMessage(ayud, channel);
+	if (msg==='!help'){
+		var ayud ={
+			as_user: true,
+			attachments: [
+				{
+					color:"danger",
+					text:"*Comandos disponibles:*",
+					fallback: "",
+					mrkdwn_in:["text"]
+				},
+				{
+					color:"#ADFF2F",
+					text:"*Buenas:* EGC_bot te saludará cordialmente.",
+					fallback: "",
+					mrkdwn_in:["text"]
+				},
+				{
+					color:"#ADFF2F",
+					text:"*!wiki:* Enlaces a cada uno de los apartados de la wiki.  ",
+					fallback: "",
+					mrkdwn_in:["text"]
+				},
+				{
+					color:"#ADFF2F",
+					text:"*!polls:* Listado de las encuestas disponibles con su identificativo.",
+					fallback: "",
+					mrkdwn_in:["text"]
+				},
+				{
+					color:"#ADFF2F",
+					text:"*¿poll x:* Listado de respuestas para la encuesta número 'x'. ",
+					fallback: "",
+					mrkdwn_in:["text"]
+				}
+				
+			]
+		};
+
+		return	web.chat.postMessage(message.channel, '', ayud);
 	}		
-			
+	
+	if (msg=='!wiki'){
+
+		var wiki ={
+			as_user: true,
+			  attachments: [
+				{
+					color:"#ff9900",
+					text:"WIKI EGC",
+					fallback: "",
+					actions: [
+						{
+							type: "button",
+							text: "Inicio",
+							url: "https://1984.lsi.us.es/wiki-egc/index.php/2017/2018",
+							style: "primary"
+						},
+						{
+							type: "button",
+							text: "Jornadas",
+							style: "primary",
+							url: "https://1984.lsi.us.es/wiki-egc/index.php/Jornadas_EGC_-_17/18"
+						},
+						{
+							type: "button",
+							text: "Teoría",
+							style: "primary",
+							url: "https://1984.lsi.us.es/wiki-egc/index.php/Teor%C3%ADa_-_17/18"
+						},
+						{
+							type: "button",
+							text: "Trabajo",
+							style: "primary",
+							url: "https://1984.lsi.us.es/wiki-egc/index.php/Trabajo_-_17/18"
+						},
+						{
+							type: "button",
+							text: "Notas",
+							style: "primary",
+							url: "https://1984.lsi.us.es/wiki-egc/index.php/Notas_-_17/18"
+						}
+					]
+				}
+			]
+		};
+
+		return	web.chat.postMessage(message.channel, '', wiki);
+	}
 });
 
 
