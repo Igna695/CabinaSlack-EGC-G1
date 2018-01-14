af88e1ebc7348d070a2f58bff589d1f9dded6c90
Igna695 <igna695@gmail.com>
2018-01-14 13:53:19 +0100

    Añadido el test para el comando Buenas

--- testArchive.js
+++ testArchive.js
@@ -4,7 +4,7 @@ var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 var {WebClient} = require('@slack/client');
-var API = process.env.SLACK_TOKEN
+var API = process.env.SLACK_TOKEN;
 var ayud = "Estos son los comandos disponibles en el bot: \n\n *Buenas:* el bot te saludará cordialmente. \n *!polls:* muestra un listado de las encuestas disponibles junto con su id. \n *¿poll x:* Muestra el listado de preguntas para una encuesta. x se corresponde con el id de la encuesta.";
 
 
@@ -14,6 +14,13 @@ rtm.start();
 
 var web = new WebClient(API);
 let channel;
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+    for (const c of rtmStartData.channels) {
+        if (c.is_member && c.name ==='testbot') { channel = c.id }
+    }
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
+  });
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -35,13 +42,25 @@ connection.connect(function(err) {
    console.log('TEST OK');
    });
 
-
-//Test de selección de todas las polls
-connection.query('SELECT * FROM poll', function(err, rows, fields){
+ //Test de selección de todas las polls
+ connection.query('SELECT * FROM poll', function(err, rows, fields){
     console.log('\nTest de select en la base de datos');
     if(err){
         console.error('Error al hacer SELECT de la base de datos'+ err.stack);
         console.log('TEST FALLIDO');
     } 
     console.log('TEST OK');
+});
+
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function(message) {
+var msg;
+
+    msg = 'Buenas';
+    rtm.sendMessage('Buenas', channel);
+    //Test del comando Buenas/Hola
+    if(msg==='Buenas' || msg==='Hola'){
+        console.log('\nTest del comando Buenas');
+        rtm.sendMessage('Buenas. Espero que tenga un buen día', channel);
+        console.log('TEST OK');
+        }
 });
