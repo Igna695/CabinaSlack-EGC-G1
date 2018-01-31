a0e808ab85816194afe09460abc94fad5882a191
Igna695 <igna695@gmail.com>
2018-01-31 19:42:30 +0100

    Añadido archivo para automatizar la creación y subida de la imagen docker

---
+++ build_and_upload_docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_BRANCH" = "master" ]
+	then
+		docker build -t bot_slack .
+
+		export DOCKER_ID_USER=$DOCKER_USER
+
+		docker login --username=$DOCKER_USER --password=$DOCKER_PASSWORD
+
+		docker tag bot_slack $DOCKER_ID_USER/bot_slack
+
+		docker push $DOCKER_ID_USER/bot_slack
+
+fi
+
+if [ "$TRAVIS_BRANCH" = "develop" ]
+	then
+		echo "No es la rama master."
+
+fi
